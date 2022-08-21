3142882d7198b8a232f599818b0affef246ad5de
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-21 09:04:56 +0200

    Add files via upload

---
+++ Styling React with CSS Classes/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+//In this framework, insted of class, it is used ClassName as the followed example, in order to apply styling
+function Footer() {
+    return (
+        <footer className="footer">This is the footer</footer>
+    )
+}
+
+export default Footer
